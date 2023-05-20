687165c55322cc0d5db3abf3366020fb5e68f7dc
MaxiBrocato <maximilianobrocato@gmail.com>
2023-05-20 01:07:26 -0300

    haciendo cagada

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ document.addEventListener("DOMContentLoaded", function() {
 });
   
   let gifs = [
-    "/gif/tumblr_895a918373adcb324eed736156585391_c4b2730c_400.gif",
-    "/gif/tumblr_29e8b358b23e3f5acca3efe237cd082b_ec564d54_400.gif",
-    "/gif/Spr_tv_tumble_0.gif",
-    "/gif/Spr_tv_ghost_0.gif",
-    "/gif/Spr_tv_firemouth_0.gif",
-    "/gif/Spr_tv_cheeseball_0.gif",
-    "/gif/Spr_tv_barrel_0.gif",
-    "/gif/pizza-tower-peppino-spaghetti.gif",
-    "/gif/mr-peppino-tv-hud-knight-pizza-tower-gif.gif",
-    "/gif/edvsn0j4sdqa1.gif",
-    "/gif/Peppinofireassnewhud.gif",
+    "tumblr_895a918373adcb324eed736156585391_c4b2730c_400.gif",
+    "tumblr_29e8b358b23e3f5acca3efe237cd082b_ec564d54_400.gif",
+    "Spr_tv_tumble_0.gif",
+    "Spr_tv_ghost_0.gif",
+    "Spr_tv_firemouth_0.gif",
+    "Spr_tv_cheeseball_0.gif",
+    "Spr_tv_barrel_0.gif",
+    "pizza-tower-peppino-spaghetti.gif",
+    "mr-peppino-tv-hud-knight-pizza-tower-gif.gif",
+    "edvsn0j4sdqa1.gif",
+    "Peppinofireassnewhud.gif",
   ];
   
   let gifIndex = 0;
